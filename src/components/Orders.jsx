d8463a77fecbfeb09baadd74c3726181e1cc491b
upendhar07 <107457558+upendhar07@users.noreply.github.com>
2025-03-04 01:42:57 +0000

    Lab 07 | Fullstack Prints React Part 2

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+// Orders.js
+import React, { useState, useEffect } from 'react';
 import { BASE_URL } from '../config';
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
 
-  /**
-   * TODO
-   * 1. Create a `fetchOrders` function that retrieves all orders from the database
-   * 2. Using the `useEffect` hook, update the existing `orders` state object when `fetchOrders` is complete
-   **/ 
+  const fetchOrders = async () => {
+    const response = await fetch(`${BASE_URL}/orders`);
+    const data = await response.json();
+    setOrders(data);
+  };
 
+  useEffect(() => {
+    fetchOrders();
+  }, []);
 
   return (
     <div className="center mw7 ba mv4">
@@ -25,7 +29,7 @@ const Orders = () => {
             </tr>
           </thead>
           <tbody>
-            {orders && orders.map((order) => (
+            {orders.map((order) => (
               <tr key={order._id}>
                 <td className="tl pv2">{order._id}</td>
                 <td className="tl pv2">{order.buyerEmail}</td>
